aab1c0222e9131f9248d7b2c05958a19661a774b
noahwekesa <noahwekesa0@gmail.com>
2023-12-27 23:39:15 +0300

    adding markdown

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Issue Tracker
 
+Radix themes
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
---
+++ app/issues/new/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { TextField, Button } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="title" />
+      </TextField.Root>
+      <SimpleMDE placeholder="description" />
+      <Button>Submit New Issue</Button>
+    </div>
+  );
+};
+
+export default NewIssuePage;
