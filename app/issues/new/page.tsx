9678e30a96ed948c1aeff8812407ac0f43b63293
noahwekesa <noahwekesa0@gmail.com>
2023-12-28 19:20:19 +0300

    form validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,31 @@
 "use client";
 import { TextField, Button } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { Controller, useForm } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(() => console.log(data))}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="title" />
+        <TextField.Input placeholder="title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="description" />
+      <Controller
+        name="description"
+        control={control}
+        render={(field) => <SimpleMDE placeholder="description" {...field} />}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
